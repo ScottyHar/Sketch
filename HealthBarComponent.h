ac677152157a33ccdb54fc23e693de521e8d8c9e
ScottyHar <toolmanproductions@gmail.com>
2026-04-20 07:16:43 +1000

    Create HealthBarComponent.h

---
+++ HealthBarComponent.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "HealthBarComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API UHealthBarComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
