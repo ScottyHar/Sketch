89b3bc87021965df81c1b6fa60659c9baeab1733
ScottyHar <toolmanproductions@gmail.com>
2026-04-20 09:44:37 +1000

    Add TakeDamage method to AEnemy class
    how unreal applys damage

--- Enemy.h
+++ Enemy.h
@@ -7,7 +7,9 @@ class UAttributeComponent;//added the attribute component
 UCLASS()
 class SLASH_API AEnemy : public ACharacter, public IHitInterface
 @@ -26,6 +27,9 @@ class SLASH_API AEnemy : public ACharacter, public IHitInterface
-
+public
+	//one way unreal can apply damage
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 private:
 	//makes a variable to the attribute component visible in blueprint defaults and instance details the world
 	UPROPERTY(VisibleAnywhere)
