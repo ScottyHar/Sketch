7711e05be994d998e9c7ee41671deb2fe1e865f4
ScottyHar <toolmanproductions@gmail.com>
2026-04-20 09:05:51 +1000

    Update HealthBarComponent.h

--- HealthBarComponent.h
+++ HealthBarComponent.h
@@ -12,5 +12,12 @@ UCLASS()
 class SLASH_API UHealthBarComponent : public UWidgetComponent
 {
 	GENERATED_BODY()
-	
+    // create a function to set health percent it will be a float named percent
+	public:
+	void SetHealthPercent(float Percent);
+
+	private:
+	//create pointer to UHealthBar named Health bar widget
+	UPROPERTY()
+	class UHealthBar* HealthBarWidget;
 };
