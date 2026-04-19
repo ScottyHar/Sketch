1015267fe1ce4fa02474f2893102076f72e4cea0
ScottyHar <toolmanproductions@gmail.com>
2026-04-20 09:11:16 +1000

    Update HealthBarComponent.cpp

--- HealthBarComponent.cpp
+++ HealthBarComponent.cpp
@@ -5,10 +5,10 @@
 void UHealthBarComponent::SetHealthPercent(float Percent)
 {
 	if (HealthBarWidget == nullptr)
-	{
+	{// caching the return of a cast to UHEalth and Returns `UUserWidget*, avoid repeated casting expensive
 		HealthBarWidget = Cast<UHealthBar>(GetUserWidgetObject());
 	}
-	
+	//update progress bar
 	if (HealthBarWidget && HealthBarWidget->HealthBar)
 	{
 		HealthBarWidget->HealthBar->SetPercent(Percent);
